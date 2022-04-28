6fd21f4b5646bf9e6f1edf2221798de09ff3f79d
Simen Endsjø <simendsjo@gmail.com>
2022-04-28 20:17:18 +0200

    Render bird on screen

--- flaksefugl.lisp
+++ flaksefugl.lisp
@@ -12,6 +12,14 @@
   (:viewport-height 600)
   (:viewport-title "Flaksefugl"))
 
+(gamekit:register-resource-package :keyword
+                                   (asdf:system-relative-pathname :flaksefugl "assets/megacrash.itch.io/source/"))
+
+(gamekit:define-image :bird "Player/bird1.png")
+
+(defmethod gamekit:draw ((app flaksefugl))
+  (gamekit:draw-image (gamekit:vec2 0 0) :bird))
+
 (defun start ()
   (gamekit:start 'flaksefugl))
 
