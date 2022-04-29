3dceebe81f9b8d120d50d35dd5403e45a4bd1c1f
Simen Endsjø <simendsjo@gmail.com>
2022-04-29 20:19:30 +0200

    Shorter name for gamekit

--- flaksefugl.lisp
+++ flaksefugl.lisp
@@ -1,39 +1,40 @@
 (cl:defpackage :flaksefugl
   (:use :cl)
+  (:local-nicknames (#:gk #:trivial-gamekit))
   (:export #:flaksefugl
            #:start
            #:stop))
 
 (cl:in-package :flaksefugl)
 
-(defvar *gravity* (gamekit:vec2 0.0 -0.1))
-(defvar *speed* (gamekit:vec2 0.0 0.0))
-(defvar *pos* (gamekit:vec2 0.0 480.0))
-(defvar *flaksespeed* (gamekit:vec2 0.0 5.0))
+(defvar *gravity* (gk:vec2 0.0 -0.1))
+(defvar *speed* (gk:vec2 0.0 0.0))
+(defvar *pos* (gk:vec2 0.0 480.0))
+(defvar *flaksespeed* (gk:vec2 0.0 5.0))
 
-(gamekit:defgame flaksefugl ()
+(gk:defgame flaksefugl ()
   ()
   (:viewport-width 800)
   (:viewport-height 600)
   (:viewport-title "Flaksefugl"))
 
-(gamekit:register-resource-package :keyword
+(gk:register-resource-package :keyword
                                    (asdf:system-relative-pathname :flaksefugl "assets/megacrash.itch.io/source/"))
 
-(gamekit:define-image :bird "Player/bird1.png")
+(gk:define-image :bird "Player/bird1.png")
 
-(defmethod gamekit:post-initialize ((app flaksefugl))
-  (gamekit:bind-button :up :pressed (lambda () (setf *speed* (gamekit:add *speed* *flaksespeed*)))))
+(defmethod gk:post-initialize ((app flaksefugl))
+  (gk:bind-button :up :pressed (lambda () (setf *speed* (gk:add *speed* *flaksespeed*)))))
 
-(defmethod gamekit:draw ((app flaksefugl))
-  (gamekit:draw-image *pos* :bird))
+(defmethod gk:draw ((app flaksefugl))
+  (gk:draw-image *pos* :bird))
 
-(defmethod gamekit:act ((app flaksefugl))
-  (setf *speed* (gamekit:add *speed* *gravity*))
-  (setf *pos* (gamekit:add *pos* *speed*)))
+(defmethod gk:act ((app flaksefugl))
+  (setf *speed* (gk:add *speed* *gravity*))
+  (setf *pos* (gk:add *pos* *speed*)))
 
 (defun start ()
-  (gamekit:start 'flaksefugl))
+  (gk:start 'flaksefugl))
 
 (defun stop ()
-  (gamekit:stop))
+  (gk:stop))
