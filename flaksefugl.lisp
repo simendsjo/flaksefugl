5318ed17ba0b60cc2a99cab92511f1f9fe579aa0
Simen Endsjø <simendsjo@gmail.com>
2022-04-30 19:33:11 +0200

    Improve repl driven development

--- flaksefugl.lisp
+++ flaksefugl.lisp
@@ -10,10 +10,12 @@
 (defvar *width* 256)
 (defvar *height* 256)
 
-(defvar *gravity* (gk:vec2 0.0 -0.1))
-(defvar *speed* (gk:vec2 0.0 0.0))
-(defvar *pos* (gk:vec2 (/ *width* 2) (/ *height* 2)))
-(defvar *flaksespeed* (gk:vec2 0.0 5.0))
+;; We set these in RESET to easily restart the game and support a more repl
+;; driven development cycle
+(defvar *gravity* nil)
+(defvar *speed* nil)
+(defvar *pos* nil)
+(defvar *flaksespeed* nil)
 
 (gk:defgame flaksefugl ()
   ()
@@ -28,6 +30,8 @@
 (gk:define-image :background "Background/Background4.png")
 
 (defmethod gk:post-initialize ((app flaksefugl))
+  (reset)
+  (gk:bind-button :f5 :pressed (lambda () (reset)))
   (gk:bind-button :up :pressed (lambda () (setf *speed* (gk:add *speed* *flaksespeed*)))))
 
 (defmethod gk:draw ((app flaksefugl))
@@ -37,6 +41,12 @@
 (defmethod gk:act ((app flaksefugl))
   (setf *speed* (gk:add *speed* *gravity*))
   (setf *pos* (gk:add *pos* *speed*)))
+
+(defun reset ()
+  (setf *gravity* (gk:vec2 0.0 -0.1))
+  (setf *speed* (gk:vec2 1.0 0.0))
+  (setf *pos* (gk:vec2 (/ *width* 2) (/ *height* 2)))
+  (setf *flaksespeed* (gk:vec2 0.0 5.0)))
 
 (defun start ()
   (gk:start 'flaksefugl))
