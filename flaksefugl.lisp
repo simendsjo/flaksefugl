89127eb6ad394552c0227ed0399d097297174527
Simen Endsjø <simendsjo@gmail.com>
2022-05-09 20:07:26 +0200

    Use rect for pipe
    .. One step forward and two steps back ..
    Create only a single pipe as we refactor this a bit.

--- flaksefugl.lisp
+++ flaksefugl.lisp
@@ -31,6 +31,9 @@
 (defvar *birdsize/2* (gk:div *birdsize* 2)
   "Midpoint of bird.")
 
+(defvar *pipe-width* 32
+  "Width of a pipe. Matches the pipe image width.")
+
 ;; We set these in RESET to easily restart the game and support a more repl
 ;; driven development cycle
 (defvar *gravity* nil
@@ -58,21 +61,36 @@
 (defvar *score* nil
   "Current score.")
 
+
+;; vectors are represented as arrays, so copy will only shallow copy and we
+;; still reference the same underlying array
+(defun copy-vec2 (vec)
+  (gk:vec2 (gk:x vec) (gk:y vec)))
+
 (defvar +empty-vec2+ (gk:vec2 0 0))
 
-(defstruct rect
+;; Disable copier to create a deep copy
+(defstruct (rect (:copier nil))
   "Rectangle represented by botto-left POS and SIZE"
   (pos +empty-vec2+)
   (size +empty-vec2+))
+
+(defun copy-rect (rect)
+  (make-rect :pos (copy-vec2 (rect-pos rect))
+             :size (copy-vec2 (rect-size rect))))
 
 (defvar +empty-rect+
   (make-rect :pos (gk:vec2 0 0)
              :size (gk:vec2 0 0)))
 
-(defstruct bbox
+(defstruct (bbox (:copier nil))
   "Bounding Box (rectangle) represented by bottom-left BEG and top-right END"
   (beg +empty-vec2+)
   (end +empty-vec2+))
+
+(defun copy-bbox (bbox)
+  (make-bbox :beg (copy-vec2 (bbox-beg bbox))
+             :end (copy-vec2 (bbox-end bbox))))
 
 (defvar +empty-bbox+
   (make-bbox :beg (gk:vec2 0 0)
@@ -110,15 +128,6 @@
   "Data for level"
   space-between
   opening)
-
-(defstruct pipe
-  "Complete vertical pipe, both TOP part and BOTTOM part. Both are SIZE height,
-so BOTTOM is placed below the screen, and TOP will extend the screen to the top."
-  (bottom +empty-rect+)
-  (top +empty-rect+))
-
-(defvar *pipe-width* 32
-  "Width of a pipe. Matches the pipe image width.")
 
 (defun world->screen (world)
   "Translates a world coordinate to screen coordinate."
@@ -166,17 +175,19 @@ reasonable bounds."
 
 ;; FIXME: We draw UP TO *size* height, but don't record it. For the bottom pipe,
 ;; it might mean we collide above the pipe!
-(defun draw-pipe (pos)
+(defun draw-pipe (rect)
   "Draws a pipe starting at POS up to *SIZE* height."
-  (let ((pos (gk:vec2 (gk:x pos) (gk:y pos)))
-        (edge-height 20)
-        (mid-height 36))
+  (let* ((rect (copy-rect rect))
+         (pos (rect-pos rect))
+         (size (rect-size rect))
+         (edge-height 20)
+         (mid-height 36))
     (flet ((incy (y) (setf (gk:y pos) (+ (gk:y pos) y))))
       ;; bottom
       (gk:draw-image (world->screen pos) :pipe :origin (gk:vec2 0 80) :width *pipe-width* :height edge-height)
       (incy edge-height)
       ;; middle
-      (dotimes (i (floor (/ (- (gk:y *size*) (* 2 edge-height)) mid-height)))
+      (dotimes (i (floor (/ (- (gk:y size) (* 2 edge-height)) mid-height)))
         (gk:draw-image (world->screen pos) :pipe :origin (gk:vec2 0 100) :width *pipe-width* :height mid-height)
         (incy mid-height))
       ;; top
@@ -193,8 +204,7 @@ reasonable bounds."
       (gk:draw-image (world->screen (gk:vec2 (+ x (* (gk:x *background-size*) i)) 0)) :background)))
   ;; pipes
   (do-each (p *pipes*)
-    (draw-pipe (pipe-bottom p))
-    (draw-pipe (pipe-top p)))
+    (draw-pipe p))
   ;; bird
   (gk:draw-image (world->screen *pos*) :bird :width (gk:x *birdsize*) :height (gk:x *birdsize*))
   ;; score
@@ -211,8 +221,7 @@ reasonable bounds."
 
 (defun pipebox (pipe)
   "Bounding box for pipe starting at position PIPE."
-  (make-bbox :beg pipe
-             :end (gk:add pipe (gk:vec2 *pipe-width* (gk:y *size*)))))
+  (rect->bbox pipe))
 
 (defun birdbox ()
   "Bounding box for bird."
@@ -230,8 +239,7 @@ reasonable bounds."
       (return-from bird-collided-p t))
     ;; pipes
     (do-each (p *pipes*)
-      (when (or (intersectsp birdbox (pipebox (pipe-bottom p)))
-                (intersectsp birdbox (pipebox (pipe-top p))))
+      (when (intersectsp birdbox (pipebox p))
         (return-from bird-collided-p t)))))
 
 (defmethod gk:act ((app flaksefugl))
@@ -242,7 +250,7 @@ reasonable bounds."
           (gk:x *camera*) (- (gk:x *pos*) (gk:x *size/2*))
           *gameover* (bird-collided-p))
     (let ((last-pipe (aref *pipes* (- (array-dimension *pipes* 0) 1))))
-      (when (> (gk:x *pos*) (+ (gk:x (pipe-bottom last-pipe)) *pipe-width*))
+      (when (> (gk:x *pos*) (+ (gk:x (rect-pos last-pipe)) *pipe-width*))
         (toggle-pause)
         (setf *level-complete* t)))))
 
@@ -261,7 +269,8 @@ reasonable bounds."
              (rndbetween (+ min-between (random (- max-between min-between) *random-state*)))
              (newx (+ x rndbetween)))
         (setf x newx)
-        (setf (aref *pipes* i) (make-pipe :bottom (gk:vec2 x bottom) :top (gk:vec2 x top)))))))
+        (setf (aref *pipes* i) (make-rect :pos (gk:vec2 x bottom)
+                                          :size (gk:vec2 *pipe-width* (gk:y *size*))))))))
 
 (defun reset ()
   "Reset game."
@@ -275,7 +284,7 @@ reasonable bounds."
         *level-complete* nil
         *score* 0
         *gameover* nil
-        *pipes* (make-array 32 :element-type 'pipe))
+        *pipes* (make-array 32 :element-type 'rect))
   (make-pipes))
 
 (defun start ()
